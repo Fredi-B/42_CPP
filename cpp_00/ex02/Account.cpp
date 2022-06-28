1cd5675ef550d3d97b56e4a893a642576684554b
Fredi-B <fredibechtoldt@outlook.com>
2022-06-28 20:37:34 +0200

    init static attributes. works for _nbAccounts. others not yet tested

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "Account.hpp"
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
 Account::Account(int initial_deposit) : _amount(initial_deposit)
 {
 	this->_displayTimestamp();
