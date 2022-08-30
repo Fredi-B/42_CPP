5f90411f3bf050a862af63233c86b8fdc20a0579
Fredi-B <fredibechtoldt@outlook.com>
2022-08-30 16:29:58 +0200

    deleted some unnecessary tests

--- cpp_03/ex03/main.cpp
+++ cpp_03/ex03/main.cpp
@@ -7,14 +7,7 @@ int	main(void)
 {
 	DiamondTrap dino_trap("dino");
 	dino_trap.whoAmI();
-	ScavTrap scavi("scav");
-	std::cout << scavi.getEnergyPoints() << std::endl;
-/* 	std::cout << dino_trap.getHitPoints() << std::endl;
+	std::cout << dino_trap.getHitPoints() << std::endl;
 	std::cout << dino_trap.getEnergyPoints() << std::endl;
 	std::cout << dino_trap.getAttackDamage() << std::endl;
-	std::cout << dino_trap.ScavTrap::getEnergyPoints() << std::endl;
-	dino_trap.setEnergyPoints(dino_trap.ScavTrap::getEnergyPoints());
-	std::cout << "scav:" << dino_trap.ScavTrap::getEnergyPoints() << std::endl;
-	std::cout << "dino" << dino_trap.getEnergyPoints() << std::endl;
- */
 }
