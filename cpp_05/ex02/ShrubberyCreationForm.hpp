d04ebc4b8e6dfb129f81abb2676f9ac9508f4c46
Fredi-B <fredibechtoldt@outlook.com>
2022-09-20 20:25:00 +0200

    wrote shrubbery class. seems to work

--- cpp_05/ex02/ShrubberyCreationForm.hpp
+++ cpp_05/ex02/ShrubberyCreationForm.hpp
@@ -2,6 +2,7 @@
 # define SHRUBBERYCREATIONFORM_HPP
 
 # include <iostream>
+# include <fstream>
 # include "Form.hpp"
 
 class	ShrubberyCreationForm : public Form
@@ -10,6 +11,14 @@ public:
 	ShrubberyCreationForm();
 	ShrubberyCreationForm(const ShrubberyCreationForm &other);
 	ShrubberyCreationForm(const std::string &target);
+	~ShrubberyCreationForm();
+
+	ShrubberyCreationForm	&operator=(const ShrubberyCreationForm &other);
+
+	bool		execute(const Bureaucrat &executor) const;
+	std::string	getTarget(void) const;
+private:
+	std::string	_target;
 };
 
 
