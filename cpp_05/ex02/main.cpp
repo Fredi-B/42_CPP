0635e5af95b11af050389f0777437af0025cbc03
Fredi-B <fredibechtoldt@outlook.com>
2022-09-21 11:36:15 +0200

    added robotomy tests

--- cpp_05/ex02/main.cpp
+++ cpp_05/ex02/main.cpp
@@ -1,14 +1,20 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int	main(void)
 {
-	std::cout << std::endl << "-------shrubbery test--------" << std::endl;
-	Bureaucrat	b("bini", 137);
-	ShrubberyCreationForm	f("formi");
-	b.signForm(f);
-	b.executeForm(f);
+	// std::cout << std::endl << "-------shrubbery test--------" << std::endl;
+	// Bureaucrat	bf("bini", 137);
+	// ShrubberyCreationForm	f("formi");
+	// bf.signForm(f);
+	// bf.executeForm(f);
 
+	std::cout << std::endl << "-------robotomy test--------" << std::endl;
+	Bureaucrat	br("bini", 40);
+	RobotomyRequestForm	r("robbi");
+	br.signForm(r);
+	br.executeForm(r);
 	return (0);
 }
