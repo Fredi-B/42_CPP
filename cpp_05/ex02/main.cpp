957016f99e8f4d524a9b5c304b7b32e21a40eae0
Fredi-B <fredibechtoldt@outlook.com>
2022-09-21 11:48:05 +0200

    added presidential tests

--- cpp_05/ex02/main.cpp
+++ cpp_05/ex02/main.cpp
@@ -2,19 +2,27 @@
 #include "Form.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int	main(void)
 {
 	// std::cout << std::endl << "-------shrubbery test--------" << std::endl;
-	// Bureaucrat	bf("bini", 137);
+	// Bureaucrat				bf("bini", 137);
 	// ShrubberyCreationForm	f("formi");
 	// bf.signForm(f);
 	// bf.executeForm(f);
 
-	std::cout << std::endl << "-------robotomy test--------" << std::endl;
-	Bureaucrat	br("bini", 40);
-	RobotomyRequestForm	r("robbi");
-	br.signForm(r);
-	br.executeForm(r);
+	// std::cout << std::endl << "-------robotomy test--------" << std::endl;
+	// Bureaucrat			br("vanni", 40);
+	// RobotomyRequestForm	r("robbi");
+	// br.signForm(r);
+	// br.executeForm(r);
+
+	std::cout << std::endl << "-------presidential test--------" << std::endl;
+	Bureaucrat				bp("pauli", 26);
+	PresidentialPardonForm	p("arty");
+	bp.signForm(p);
+	bp.executeForm(p);
+
 	return (0);
 }
