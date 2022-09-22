51dd43be1a1303fec0a8f07aaab5c20063cbe8ed
Fredi-B <fredibechtoldt@outlook.com>
2022-09-22 10:44:31 +0200

    just cosmetics

--- cpp_04/ex03/main.cpp
+++ cpp_04/ex03/main.cpp
@@ -6,44 +6,49 @@
 
 int main(void)
 {
-	// IMateriaSource *src = new MateriaSource();
+	IMateriaSource *src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
+	// IMateriaSource	*src = new MateriaSource();
 	// src->learnMateria(new Ice());
 	// src->learnMateria(new Cure());
 
-	// ICharacter* me = new Character("me");
-	
-	// AMateria* tmp;
-	// tmp = src->createMateria("ice");
-	// me->equip(tmp);
+	// AMateria	*tmp;
 	// tmp = src->createMateria("cure");
-	// me->equip(tmp);
-	
-	// ICharacter* bob = new Character("bob");
-	// me->use(0, *bob);
-	// me->use(1, *bob);
-	// delete bob;
-	// delete me;
+	// AMateria	*tmp2;
+	// tmp2 = src->createMateria("ice");
+
+	// Character	*bob = new Character("bob");
+	// bob->equip(tmp);
+	// bob->equip(tmp2);
+
+	// Character	*jim = new Character("jim");
+	// *jim = *bob;
+
+	// jim->use(0, *bob);
+	// bob->use(0, *jim);
+	// bob->use(1, *jim);
+
 	// delete src;
-
-	Character	*bob = new Character("bob");
-	IMateriaSource	*src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	AMateria	*tmp;
-	tmp = src->createMateria("cure");
-	AMateria	*tmp2;
-	tmp2 = src->createMateria("ice");
-	bob->equip(tmp);
-	bob->equip(tmp2);
-	Character	*jim = new Character("jim");
-	*jim = *bob;
-	jim->use(0, *bob);
-	bob->use(0, *jim);
-	bob->use(1, *jim);
-	delete bob;
-	delete src;
-	delete jim;
-
-	// system("leaks interface");
+	// delete bob;
+	// delete jim;
+	
 	return 0;
 }
