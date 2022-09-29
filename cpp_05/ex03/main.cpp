7943b649eacb8ce3c4a84082aec1c62b4770080a
Fredi-B <fredibechtoldt@outlook.com>
2022-09-29 14:29:21 +0200

    put everything in try catch block

--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -7,38 +7,47 @@
 
 int	main(void)
 {
-	Bureaucrat	b("bini", 5);
-	Intern		someRandomIntern;
-
-	std::cout << std::endl << "-----------robotomy test---------" << std::endl;
-	Form		*rrf;
-	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-	b.signForm(*rrf);
-	b.executeForm(*rrf);
-	delete rrf;
-
-	std::cout << std::endl  << "-----------shrubbery test---------" << std::endl;
-	Form		*scf;
-	scf = someRandomIntern.makeForm("shrubbery creation", "Hill");
-	b.signForm(*scf);
-	b.executeForm(*scf);
-	delete scf;
-
-	std::cout << std::endl  << "-----------presidential test---------" << std::endl;
-	Form		*ppf;
-	ppf = someRandomIntern.makeForm("presidential pardon", "Arty");
-	b.signForm(*ppf);
-	b.executeForm(*ppf);
-	delete ppf;
-
-	std::cout << std::endl  << "-----------random form test---------" << std::endl;
-	Form		*rff;
-
-	rff = someRandomIntern.makeForm("random form", "Arty");
-	if (rff)
+	try
 	{
-		b.signForm(*rff);
-		b.executeForm(*rff);
+	
+		Bureaucrat	b("bini", 5);
+		Intern		someRandomIntern;
+
+		std::cout << std::endl << "-----------robotomy test---------" << std::endl;
+		Form		*rrf;
+		rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+		b.signForm(*rrf);
+		b.executeForm(*rrf);
+		delete rrf;
+
+		std::cout << std::endl  << "-----------shrubbery test---------" << std::endl;
+		Form		*scf;
+		scf = someRandomIntern.makeForm("shrubbery creation", "Hill");
+		b.signForm(*scf);
+		b.executeForm(*scf);
+		delete scf;
+
+		std::cout << std::endl  << "-----------presidential test---------" << std::endl;
+		Form		*ppf;
+		ppf = someRandomIntern.makeForm("presidential pardon", "Arty");
+		b.signForm(*ppf);
+		b.executeForm(*ppf);
+		delete ppf;
+
+		std::cout << std::endl  << "-----------random form test---------" << std::endl;
+		Form		*rff;
+
+		rff = someRandomIntern.makeForm("random form", "Arty");
+		if (rff)
+		{
+			b.signForm(*rff);
+			b.executeForm(*rff);
+		}
 	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught in main(): " << e.what() << std::endl;
+	}
+
 	return (0);
 }
