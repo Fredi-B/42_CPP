998f779e6e60551c4036ba496b7951327994476a
Fredi-B <fredibechtoldt@outlook.com>
2022-09-29 14:24:26 +0200

    put tests in try catch block

--- cpp_05/ex02/main.cpp
+++ cpp_05/ex02/main.cpp
@@ -6,23 +6,29 @@
 
 int	main(void)
 {
-	std::cout << std::endl << "-------shrubbery test--------" << std::endl;
-	Bureaucrat				bf("bini", 137);
-	ShrubberyCreationForm	f("formi");
-	bf.signForm(f);
-	bf.executeForm(f);
+	try
+	{
+		std::cout << std::endl << "-------shrubbery test--------" << std::endl;
+		Bureaucrat				bf("bini", 137);
+		ShrubberyCreationForm	f("formi");
+		bf.signForm(f);
+		bf.executeForm(f);
 
-	std::cout << std::endl << "-------robotomy test--------" << std::endl;
-	Bureaucrat			br("vanni", 40);
-	RobotomyRequestForm	r("robbi");
-	br.signForm(r);
-	br.executeForm(r);
+		std::cout << std::endl << "-------robotomy test--------" << std::endl;
+		Bureaucrat			br("vanni", 40);
+		RobotomyRequestForm	r("robbi");
+		br.signForm(r);
+		br.executeForm(r);
 
-	std::cout << std::endl << "-------presidential test--------" << std::endl;
-	Bureaucrat				bp("pauli", 26);
-	PresidentialPardonForm	p("arty");
-	bp.signForm(p);
-	bp.executeForm(p);
-
+		std::cout << std::endl << "-------presidential test--------" << std::endl;
+		Bureaucrat				bp("pauli", 4);
+		PresidentialPardonForm	p("arty");
+		bp.signForm(p);
+		bp.executeForm(p);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught in main(): " << e.what() << std::endl;
+	}
 	return (0);
 }
